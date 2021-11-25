feeb1afe70b33af7201cc901b6e188f5d02aa462
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-11-25 17:10:15 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 756
+const colors = ['red', 'blue', 'orange', 'purple', 'pink', 'aquamarine', 'SkyBlue', 'yellow']
+
+for (let i = 0; i< SQUARES_NUMBER; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element){
+    const color = getColor()
+
+    element.style.backgroundColor = color
+
+    element.style.boxShadow = `0 0 2px ${color}, 0, 0, 10px ${color}`
+}
+
+function removeColor(element){
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000 `
+}
+
+function getColor(){
+    const index =  Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
